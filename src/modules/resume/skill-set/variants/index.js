0c657424483f333386d687f90939ef0bf6b7a4ee
bhavay <bhavaypuri15@gmail.com>
2020-07-28 20:05:00 +0530

    Add percentage variant in skill-set

--- src/modules/resume/skill-set/variants/index.js
+++ src/modules/resume/skill-set/variants/index.js
@@ -1,9 +1,11 @@
 import Basic from './basic/index.js';
 import Advanced from './advanced/index.js';
+import Column from './column';
 import Percentage from './percentage/index';
 
 export default {
   Basic,
   Advanced,
+  Column,
   Percentage
 }
